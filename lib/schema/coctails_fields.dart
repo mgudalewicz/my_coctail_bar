ac85daa9f96ba61d59248598e2b3e586a8b95bef
mgudalewicz <michal.gudalewicz@gmail.com>
2022-10-27 03:23:36 +0200

    add schema

---
+++ lib/schema/coctails_fields.dart
@@ -0,0 +1,13 @@
+class CoctailsFields {
+  /// Type [String]
+  static const String id = 'id';
+
+  /// Type [String]
+  static const String name = 'name';
+
+  /// Type [int]
+  static const String strength = 'strength';
+
+  /// Type [String]
+  static const String recipe = 'recipe';
+}
